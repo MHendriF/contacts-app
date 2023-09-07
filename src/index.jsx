0dd46c3b9affab9c59525dc7b5d953ee6c9567c7
MHendriF <hendrifebriansyah28@gmail.com>
2023-09-07 12:48:51 +0700

    memberikan styling

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import ContactApp from './ContactApp';
+import ContactApp from './components/ContactApp';
 
-const element = <h1>Hello, world!</h1>;
+// styling
+import './styles/style.css';
  
 const root = createRoot(document.getElementById('root'));
 root.render(<ContactApp />);
