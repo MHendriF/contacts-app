51e920f3776f60d26e10862e75debc2cca2558e4
MHendriF <hendrifebriansyah28@gmail.com>
2023-09-12 10:45:19 +0700

    penerapan PropTypes untuk validasi data

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ContactList from './ContactList';
 import { getData } from '../utils/data';
 import ContactInput from './ContactInput';
