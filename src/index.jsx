a159cd3891d4f42b6cbcb5fa6684aeb07b629df3
MHendriF <hendrifebriansyah28@gmail.com>
2023-09-13 13:26:31 +0700

    implementasi react router

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import ContactApp from './components/ContactApp';
 
@@ -6,4 +7,8 @@ import ContactApp from './components/ContactApp';
 import './styles/style.css';
  
 const root = createRoot(document.getElementById('root'));
-root.render(<ContactApp />);
+root.render(
+    <BrowserRouter>
+     <ContactApp />
+   </BrowserRouter>
+  );
